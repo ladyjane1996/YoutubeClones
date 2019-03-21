265f49b43c4644286517df09b8618ed12d709161
lady jane <ljgc096@gmail.com>
2019-03-21 16:21:08 -0600

    Adding the modify FIle

--- RPS.js
+++ RPS.js
@@ -1,7 +1,56 @@
-'use Strict'
 alert("WELCOME TO THE GAME");
 alert ("No, why winter is comming, is why Winter is here!!");
-alert("1");
-alert("2");
 alert("3");
-alert("ROCK PAPER OR SCISSORS?");
+alert("2");
+alert("1");
+
+var playerScore = 0;
+var computerScore = 0;
+var tiedScore =0;
+var cont = 0; 
+
+function computerPlay() {
+  let pcSelect = Math.floor(Math.random()*3);
+  if (pcSelect === 0 ) {
+    let selection = "Rock!"; 
+  } else if (pcSelect === 1) {
+    let selection = "Paper!"; 
+  } else {
+    let selection = "Scissors!"; 
+  }
+
+  return Selection;
+}
+//Computer Choice! 
+function playRound (PlayerSelection,computerSelection) 
+    let result =  "";
+
+    if (playerSelection === computerSelection) {
+        tiedScore++;
+        result = "TIED!"; 
+
+     } else if (playerSelection === "rock"&& computerSelection === "Scissors!" || playerSelection === "paper"&& computerSelection === "Rock!" 
+     || playerSelection === "scissors"&& computerSelection === "Paper!"  ) {
+        playerScore++;
+        result = "YOU WIN!"; 
+     } else {
+        computerScore++;
+        result = "Loser!"; 
+     }
+   } 
+
+function game ()  {
+
+    var Selection = prompt("ROCK PAPER OR SCISSORS?");
+    var playerSelection = Selection.toLowerCase();
+
+    do {
+        cont = cont + 1; 
+        playRound();
+        
+    } while (cont = 5)            
+    } 
+
+  const playerSelection = "";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection,computerSelection));
